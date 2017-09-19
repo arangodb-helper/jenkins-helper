f87d4a91d7ee6b623ff6e220c07e02f2e346f365
Frank Celler <frank@arangodb.com>
2017-09-19 14:53:48 +0200

    cluster and edition

--- start/server.sh
+++ start/server.sh
@@ -1,25 +1,48 @@
-echo "NAME: $ARANGO_DOCKER_NAME"
-echo "PORT: $ARANGO_PORT"
-echo "MODE: $ARANGO_MODE"
-echo "ENGINE: $ARANGO_STORAGE_ENGINE"
-echo
-
-for name in ARANGO_DOCKER_NAME ARANGO_PORT ARANGO_MODE ARANGO_STORAGE_ENGINE; do
+for name in ARANGO_DOCKER_NAME ARANGO_PORT ARANGO_MODE ARANGO_STORAGE_ENGINE ARANGODB_EDITION; do
     if [ -z "$((name))" ]; then
         echo "$name missing"
         exit 1
     fi
 done
 
+echo "NAME: $ARANGO_DOCKER_NAME"
+echo "PORT: $ARANGO_PORT"
+echo "MODE: $ARANGO_MODE"
+echo "ENGINE: $ARANGO_STORAGE_ENGINE"
+echo "EDITION: $ARANGO_STORAGE_EDITION"
+echo
+
+docker kill $ARANGO_DOCKER_NAME > /dev/null 2>&1 || true
 docker rm -fv $ARANGO_DOCKER_NAME > /dev/null 2>&1 || true
 
-docker run \
-    --name=$ARANGO_DOCKER_NAME \
-    -d \
-    -p $ARANGO_PORT:8529 \
-    -e ARANGO_ROOT_PASSWORD=$ARANGO_ROOT_PASSWORD \
-    -e ARANGO_STORAGE_ENGINE=$ARANGO_STORAGE_ENGINE \
-    c1.triagens-gmbh.zz:5000/arangodb/enterprise-maintainer:feature-branch-docker-images
+if [ "$ARANGO_MODE" == "cluster" ]; then
+    JWTFILE="`pwd`/jwtsecret.$$"
+    rm -f $JWTFILE
+
+    echo "geheim" > $JWTFILE
+
+    docker run \
+        --name=$ARANGO_DOCKER_NAME \
+        -d \
+        -v $JWTFILE:jwtsecret:ro \
+        -p $ARANGO_PORT:8529 \
+        -e ARANGO_ROOT_PASSWORD=$ARANGO_ROOT_PASSWORD \
+        c1.triagens-gmbh.zz:5000/arangodb/${ARANGODB_EDITION}-maintainer:feature-branch-docker-images \
+        arangodb --starter.local --server.storage-engine $ARANGO_STORAGE_ENGINE --starter.data-dir testrun
+
+    rm -f $JWTFILE
+elif [ "$ARANGO_MODE" == "singleserver" ]; then
+    docker run \
+        --name=$ARANGO_DOCKER_NAME \
+        -d \
+        -p $ARANGO_PORT:8529 \
+        -e ARANGO_ROOT_PASSWORD=$ARANGO_ROOT_PASSWORD \
+        -e ARANGO_STORAGE_ENGINE=$ARANGO_STORAGE_ENGINE \
+        c1.triagens-gmbh.zz:5000/arangodb/${ARANGODB_EDITION}-maintainer:feature-branch-docker-images
+else
+    echo "unknown mode $ARANGO_MODE"
+    exit 1
+fi
 
 trap "docker rm -fv $ARANGO_DOCKER_NAME" EXIT
 
