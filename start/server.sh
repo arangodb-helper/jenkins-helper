6067f8923bc19962fa1451138ed387e6529c7170
Frank Celler <frank@arangodb.com>
2017-09-19 14:21:23 +0200

    sanity checks

--- start/server.sh
+++ start/server.sh
@@ -1,3 +1,9 @@
+echo "NAME: $ARANGO_DOCKER_NAME"
+echo "PORT: $ARANGO_PORT"
+echo "MODE: $ARANGO_MODE"
+echo "ENGINE: $ARANGO_STORAGE_ENGINE"
+echo
+
 for name in ARANGO_DOCKER_NAME ARANGO_PORT ARANGO_MODE ARANGO_STORAGE_ENGINE; do
     if [ -z "$((name))" ]; then
         echo "$name missing"
@@ -5,19 +11,14 @@ for name in ARANGO_DOCKER_NAME ARANGO_PORT ARANGO_MODE ARANGO_STORAGE_ENGINE; do
     fi
 done
 
-docker rm -fv $ARANGO_DOCKER_NAME 2>&1 > /dev/null || true
-
-echo "PORT: $ARANGO_PORT"
-echo "MODE: $ARANGO_MODE"
-echo "ENGINE: $ARANGO_STORAGE_ENGINE"
-echo
+docker rm -fv $ARANGO_DOCKER_NAME > /dev/null 2>&1 || true
 
 docker run \
     --name=$ARANGO_DOCKER_NAME \
     -d \
     -p $ARANGO_PORT:8529 \
     -e ARANGO_ROOT_PASSWORD=$ARANGO_ROOT_PASSWORD \
-    -e ARANGO_STORAGE_ENGINE=$ARANGO_STORAGE_ENGINE
+    -e ARANGO_STORAGE_ENGINE=$ARANGO_STORAGE_ENGINE \
     c1.triagens-gmbh.zz:5000/arangodb/enterprise-maintainer:feature-branch-docker-images
 
 trap "docker rm -fv $ARANGO_DOCKER_NAME" EXIT
