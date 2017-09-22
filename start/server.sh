1881c731bd2fee116883bd557c47789b0885d057
Frank Celler <frank@arangodb.com>
2017-09-22 09:41:47 +0200

    added debug output

--- start/server.sh
+++ start/server.sh
@@ -20,6 +20,7 @@ echo
 docker kill $ARANGO_DOCKER_NAME > /dev/null 2>&1 || true
 docker rm -fv $ARANGO_DOCKER_NAME > /dev/null 2>&1 || true
 docker pull c1.triagens-gmbh.zz:5000/arangodb/linux-${ARANGO_EDITION}-maintainer:devel
+docker run c1.triagens-gmbh.zz:5000/arangodb/linux-${ARANGO_EDITION}-maintainer:devel arangosh --version
 
 if [ "$ARANGO_MODE" == "cluster" ]; then
     if [ "$ARANGO_AUTH" == "auth" ]; then
