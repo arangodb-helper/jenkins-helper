aab0e33074d336872fae9f89d0f8f98359e2b0e1
Frank Celler <frank@arangodb.com>
2017-09-26 15:59:19 +0200

    guess a port

--- start/adjust_port.sh
+++ start/adjust_port.sh
@@ -1,18 +1,5 @@
-if test $engine == "mmfiles"; then
-  port=`expr $port + 1`
-fi
-
-if test $mode == "cluster"; then
-  port=`expr $port + 2`
-fi
-
-if test $edition == "enterprise"; then
-  port=`expr $port + 4`
-fi
-
-if test $auth == "auth"; then
-  port=`expr $port + 8`
-fi
+port=`./jenkins/start/port.sh`
+trap "./jenkins/start/port.sh --clean $port" EXIT
 
 export ARANGO_STORAGE_ENGINE=$engine
 export ARANGO_MODE=$mode
