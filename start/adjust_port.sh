1e719729ea56252ed3ac500ad9e148b597cecff6
Frank Celler <frank@arangodb.com>
2017-09-26 16:00:02 +0200

    guess a port

--- start/adjust_port.sh
+++ start/adjust_port.sh
@@ -1,6 +1,8 @@
 port=`./jenkins/start/port.sh`
 trap "./jenkins/start/port.sh --clean $port" EXIT
 
+echo "using port $port"
+
 export ARANGO_STORAGE_ENGINE=$engine
 export ARANGO_MODE=$mode
 export ARANGO_EDITION=$edition
