d2225f1f44c24dd811b490faf3c071656ce8b606
KVS85 <vadim@arangodb.com>
2019-05-10 16:13:28 +0300

    Fix typo

--- start/adjust_cluster_ports.sh
+++ start/adjust_cluster_ports.sh
@@ -5,9 +5,9 @@ port3=`./jenkins/start/port.sh`
 echo "using ports $port1, $port2, $port3"
 
 clean_ports () {
-  [ -z "$port1" ] && ./jenkins/start/port.sh --clean $port1
-  [ -z "$port2" ] && ./jenkins/start/port.sh --clean $port2
-  [ -z "$port3" ] && ./jenkins/start/port.sh --clean $port3
+  [ ! -z "$port1" ] && ./jenkins/start/port.sh --clean $port1
+  [ ! -z "$port2" ] && ./jenkins/start/port.sh --clean $port2
+  [ ! -z "$port3" ] && ./jenkins/start/port.sh --clean $port3
 }
 
 trap clean_ports EXIT
