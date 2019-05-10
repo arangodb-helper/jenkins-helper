1693a04e4342570034f201dcb3f60eb4089654d3
KVS85 <vadim@arangodb.com>
2019-05-10 16:02:22 +0300

    Remove multiple traps

--- start/adjust_cluster_ports.sh
+++ start/adjust_cluster_ports.sh
@@ -1,13 +1,16 @@
 port1=`./jenkins/start/port.sh`
-trap "./jenkins/start/port.sh --clean $port1" EXIT
-
 port2=`./jenkins/start/port.sh`
-trap "./jenkins/start/port.sh --clean $port2" EXIT
-
 port3=`./jenkins/start/port.sh`
-trap "./jenkins/start/port.sh --clean $port3" EXIT
 
 echo "using ports $port1, $port2, $port3"
+
+clean_ports () {
+  [ -z "$port1" ] && ./jenkins/start/port.sh --clean $port1
+  [ -z "$port2" ] && ./jenkins/start/port.sh --clean $port2
+  [ -z "$port3" ] && ./jenkins/start/port.sh --clean $port3
+}
+
+trap clean_ports EXIT
 
 export ARANGO_STORAGE_ENGINE=$engine
 export ARANGO_MODE=$mode
